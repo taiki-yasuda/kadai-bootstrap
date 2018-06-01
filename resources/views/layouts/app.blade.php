c244b98825655f6920b4e42556c384764603390e
Jiro.kirameki <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-06-01 07:58:12 +0000

    bootstrap design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>MessageBoard</title>
         
          <!-- Bootstrap -->
@@ -15,26 +16,6 @@
     
 
     <body>
-        <header>
-    <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="/">MessageBoard</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', '新規メッセージの投稿') !!}</li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</header>
         @include('commons.navbar')
         
         <div class="container">
