9ed5cc1842c18ee1febfd86f7e574829c5c29513
Jiro.kirameki <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-06-04 06:50:51 +0000

    heroku-kadai-edit

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,9 +12,15 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.store', 'Home') !!}</li>
-                    <li>{!! link_to_route('tasks.store', 'Index') !!}</li>
-                    <li>{!! link_to_route('tasks.create', 'Add new tasks') !!}</li>
+                    <li><button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-plane"></span>
+                        {!! link_to_route('tasks.store', 'Home') !!}</li></button>
+                        <li><button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-th-list"></span>
+                        {!! link_to_route('tasks.store', 'Index') !!}</li></button>
+                        <li><button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-edit"></span>
+                        {!! link_to_route('tasks.create', 'Add new tasks') !!}</li></button>
                 </ul>
             </div>
         </div>
