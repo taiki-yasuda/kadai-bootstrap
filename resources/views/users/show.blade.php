44473aa97f031a0b147d140ea5d8179df44f9a06
Jiro.kirameki <ec2-user@ip-172-31-19-4.us-east-2.compute.internal>
2018-06-07 02:02:35 +0000

    post bootstrap

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -22,11 +22,13 @@
             @if (Auth::user()->id == $user->id)
                   {!! Form::open(['route' => 'tasks.store']) !!}
                       <div class="form-group">
-                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'placeholder' => "write content", 'rows' => '2']) !!}
+                          {!! Form::textarea('status', old('status'), ['class' => 'form-control', 'placeholder' => "write status", 'rows' => '1']) !!}
                           {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
                       </div>
                   {!! Form::close() !!}
             @endif
+            
             @if (count($tasks) > 0)
                 @include('tasks.tasks', ['tasks' => $tasks])
             @endif
